0d15816f9673d299859b90d5c9cceeb45ca1a467
Julia Lindman <julialindmanmusic@gmail.com>
2022-06-02 13:39:23 +0200

    Adding three numbers together

--- src/main/java/org/example/Three.java
+++ src/main/java/org/example/Three.java
@@ -6,7 +6,8 @@ public class Three {
     public static void main (String[] args){
         int One = 3;
         int Two = 5;
-        System.out.println (One + Two);
+        int Three = 8;
+        System.out.println (One + Two + Three);
 
     }
 }
