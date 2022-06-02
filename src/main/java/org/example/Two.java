b9359cfc5a19783283e7d356eda5ab060a7c96a6
Julia Lindman <julialindmanmusic@gmail.com>
2022-06-02 20:04:20 +0200

    Avarage of three numbers

--- src/main/java/org/example/Two.java
+++ src/main/java/org/example/Two.java
@@ -1,11 +1,12 @@
 package org.example;
-
-import jdk.nashorn.internal.parser.Scanner;
+import java.util.*;
 
 public class Two {
 
     public static void main (String[] args) {
-        int number = 34;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int number=sc.nextInt();
         int rest = number % 4;
 
         if (rest == 0)
